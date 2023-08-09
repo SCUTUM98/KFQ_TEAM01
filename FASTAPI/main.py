177d7a1f846b368744898f06272a70aabdd54b80
BONGEUNGU <rnqhsrms98@gmail.com>
2023-08-09 11:22:38 +0900

    fix code architecture & test converting json type

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
+import json
+import pandas as pd
+import os
+import glob
+
 app = FastAPI()
 
 @app.get("/")
-def 이름():
+def name():
   return '보낼 값'
 
 @app.get("/two")
 def car():
-  import pandas as pd
-  import os
-  import glob
 #파일경로는 개인별로 바꿔주세요
-  folder_path = r'C:/work/python/python_basic/KFQ_TEAM01-1/yolo/results/xlsxdata'
+  folder_path = r'/Users/bongeungu/Desktop/kfq/KFQ_TEAM01/yolo/results/xlsxdata'
   excel_files = glob.glob(os.path.join(folder_path, '*.csv'))
   excel_files = [path.replace("\\", "/") for path in excel_files]
 
@@ -25,7 +27,21 @@ def car():
           new_df=pd.read_csv(file_name)
           new_df = new_df.to_dict('records')
           df.append(new_df)
-  return df
-
-
   
+  return df
+  
+  """
+  merged_data=[]
+  for i, file_name in enumerate(excel_files):
+    if i == 0:
+        df = pd.read_csv(file_name)
+        merged_data.extend(df.to_dict('records'))
+    else:
+        new_df = pd.read_csv(file_name)
+        merged_data.extend(new_df.to_dict('records'))
+
+  # 리스트 형태의 딕셔너리를 JSON으로 변환
+  json_data = json.dumps(merged_data)
+  
+  return json_data
+  """
