6b62f75f14f56e3b6460f28290133a88784876f7
hyozzin <131636227+hyozzin@users.noreply.github.com>
2023-08-10 15:36:17 +0900

    upload

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -1,47 +1,41 @@
-from fastapi import FastAPI
-import json
-import pandas as pd
-import os
-import glob
+from typing import Optional
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# description ?? => 422error
+class Item(BaseModel):
+    camera_name : str # 필수 속성으로 갖고 str인지 검사
+    time : str
+    xmin : float
+    ymin : float
+    xmax : float
+    ymax : float
+    confidence : float
+    mlass : int
+    name : str
+
 @app.get("/")
-def name():
-  return '보낼 값'
+def 환영합니다():
+  return 'Welcome!'
 
-@app.get("/two")
-def car():
-#파일경로는 개인별로 바꿔주세요.
-  folder_path = r'/Users/bongeungu/Desktop/kfq/KFQ_TEAM01/yolo/results/xlsxdata'
-  excel_files = glob.glob(os.path.join(folder_path, '*.csv'))
-  excel_files = [path.replace("\\", "/") for path in excel_files]
-
-  for i, n in enumerate(range(len(excel_files))):
-      if i == 0:
-          file_name=excel_files[n] 
-          df= pd.read_csv(file_name)
-          df = df.to_dict('records')
-      else:
-          file_name=excel_files[n] 
-          new_df=pd.read_csv(file_name)
-          new_df = new_df.to_dict('records')
-          df.append(new_df)
-  
-  return df
-  
-  """
-  merged_data=[]
-  for i, file_name in enumerate(excel_files):
-    if i == 0:
-        df = pd.read_csv(file_name)
-        merged_data.extend(df.to_dict('records'))
-    else:
-        new_df = pd.read_csv(file_name)
-        merged_data.extend(new_df.to_dict('records'))
-
-  # 리스트 형태의 딕셔너리를 JSON으로 변환
-  json_data = json.dumps(merged_data)
-  
-  return json_data
-  """
+@app.put("/items/{item_id}")
+async def update_item(
+    item: Item = Body(
+        ...,
+        example={
+            "camera_name":"DaeyaIntersection",
+            "time":"2023-08-07 10:32:20.982046",
+            "xmin":105.61141967773438,
+            "ymin":350.4307861328125,
+            "xmax":142.32504272460938,
+            "ymax":377.0431518554688,
+            "confidence":0.8104487657546997,
+            "mclass":2,
+            "name":"car",
+        },
+    ),
+):
+    results = {"item": item}
+    return results
