40655f7cd39f88962527a47acf3edb7d6ec0126d
BONGEUNGU <rnqhsrms98@gmail.com>
2023-08-15 15:27:22 +0900

    add boolean type column

--- yolo/detection.py
+++ yolo/detection.py
@@ -1,8 +1,10 @@
 def category(object_coords):
-    object_coords['action'] = 'NaN'
+    object_coords['action_detection'] = 0
+    object_coords['action_category'] = 'NaN'
     for i in range(len(object_coords)):        
         if object_coords['name'][i] == 'person':
-            object_coords['action'][i] = 'person detected'
+            object_coords['action_detection'][i] = 1
+            object_coords['action_caategory'][i] = 'person detected'
         else:
             continue
     return object_coords
