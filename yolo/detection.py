391e4c324681a1e2886f40828f229b35d6fd1ae9
BONGEUNGU <rnqhsrms98@gmail.com>
2023-08-16 11:54:47 +0900

    add fire logic

--- yolo/detection.py
+++ yolo/detection.py
@@ -4,7 +4,10 @@ def category(object_coords):
     for i in range(len(object_coords)):        
         if object_coords['name'][i] == 'person':
             object_coords['action_detection'][i] = 1
-            object_coords['action_caategory'][i] = 'person detected'
+            object_coords['action_category'][i] = 'person detected'
+        elif object_coords['name'][i] == 'Fire':
+            object_coords['action_detection'][i] = 1
+            object_coords['action_category'][i] = 'fire detected'
         else:
             continue
     return object_coords
