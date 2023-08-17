31e5841ff4a949934173bacee30350ce0ffefdfa
BONGEUNGU <rnqhsrms98@gmail.com>
2023-08-17 10:34:49 +0900

    add animal category

--- yolo/detection.py
+++ yolo/detection.py
@@ -8,6 +8,9 @@ def category(object_coords):
         elif object_coords['name'][i] == 'Fire':
             object_coords['action_detection'][i] = 1
             object_coords['action_category'][i] = 'fire detected'
+        elif object_coords['name'][i] == 'Cat' or 'Deer' or 'Dog' or 'Racoon' or 'WildBoar':
+            object_coords['action_detection'][i] = 1
+            object_coords['action_category'][i] = f'animal detected({object_coords["name"][i]})'
         else:
             continue
     return object_coords
