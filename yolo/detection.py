201ef30cbd858892393150094193a012a7ca529f
BONGEUNGU <rnqhsrms98@gmail.com>
2023-08-17 14:00:39 +0900

    add Smoke action

--- yolo/detection.py
+++ yolo/detection.py
@@ -3,9 +3,9 @@ def category(object_coords):
         if object_coords['Class_Name'][i] == 'person':
             object_coords['action_detection'][i] = 1
             object_coords['action_category'][i] = 'person detected'
-        elif object_coords['Class_Name'][i] == 'Fire':
+        elif object_coords['Class_Name'][i] in ['Fire', 'Smoke']:
             object_coords['action_detection'][i] = 1
-            object_coords['action_category'][i] = 'fire detected'
+            object_coords['action_category'][i] = f'{object_coords["Class_Name"][i]} detected'
         elif object_coords['Class_Name'][i] in ['cat', 'deer', 'dog', 'racoon', 'wild_boar']:
             object_coords['action_detection'][i] = 1
             object_coords['action_category'][i] = f'animal detected({object_coords["Class_Name"][i]})'
