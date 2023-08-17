172dc440a87ca8790c2050b2bd6be60282b09f87
BONGEUNGU <rnqhsrms98@gmail.com>
2023-08-17 10:47:28 +0900

    add class mapping data

---
+++ yolo/class_mapping.py
@@ -0,0 +1,22 @@
+# Object class mapping
+yolo_class_mapping = {
+    0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck',
+    8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench',
+    14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear',
+    22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'sneakers',
+    29: 'glasses', 30: 'hat', 31: 'scarf', 32: 'necktie', 33: 'bag', 34: 'bottle', 35: 'wine glass',
+    36: 'cup', 37: 'fork', 38: 'knife', 39: 'spoon', 40: 'bowl', 41: 'banana', 42: 'apple', 43: 'sandwich',
+    44: 'orange', 45: 'broccoli', 46: 'carrot', 47: 'hot dog', 48: 'pizza', 49: 'donut', 50: 'cake',
+    51: 'chair', 52: 'couch', 53: 'potted plant', 54: 'bed', 55: 'dining table', 56: 'toilet', 57: 'tv',
+    58: 'laptop', 59: 'mouse', 60: 'remote', 61: 'keyboard', 62: 'cell phone', 63: 'microwave', 64: 'oven',
+    65: 'toaster', 66: 'sink', 67: 'refrigerator', 68: 'book', 69: 'clock', 70: 'vase', 71: 'scissors',
+    72: 'teddy bear', 73: 'hair drier', 74: 'toothbrush'
+}
+
+fire_class_mapping = {
+    0: 'fire', 1: 'smoke'
+}
+
+animals_class_mapping = {
+    0: 'cat', 1: 'deer', 2: 'dog', 3: 'racoon', 4: 'wild_boar'
+}
