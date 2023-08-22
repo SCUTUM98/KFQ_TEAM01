e50e37eb9a9b5f6a30e5a663564f3d7a4f62eac2
BONGEUNGU <rnqhsrms98@gmail.com>
2023-08-22 16:35:20 +0900

    add road detection function

--- yolo/convert_data.py
+++ yolo/convert_data.py
@@ -1,6 +1,7 @@
 import cv2
+import pandas as pd
 
-def collect_data(results, frame, mapping, results_df, captured_time):
+def collect_data(results, frame, mapping, results_df, captured_time, road_coordinates):
     # Process the results and draw bounding boxes on the frame
     for detections in results:
         if detections is not None:
@@ -8,6 +9,8 @@ def collect_data(results, frame, mapping, results_df, captured_time):
                 # resize image size to 640x640
                 input_size = (640,640)
                 original_height, original_width, _ = frame.shape
+                
+                # coordinate correction cause img has resized to 640,640
                 x1 = int(x1 * (original_width / input_size[0]))
                 y1 = int(y1 * (original_height / input_size[1]))
                 x2 = int(x2 * (original_width / input_size[0]))
@@ -19,9 +22,33 @@ def collect_data(results, frame, mapping, results_df, captured_time):
                 thickness = 2
                 cv2.rectangle(frame, (x1, y1), (x2, y2), color, thickness)
                 cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness)
+                
+                if class_name in ['car', 'truck', 'bus']:
+                    if not ((road_coordinates['X1'] == x1) & (road_coordinates['Y1'] == y1)).any():
+                        road_coordinates = pd.concat([road_coordinates, pd.DataFrame({'X1': [x1], 'Y1': [y1], 'X2': [x2], 'Y2': [y2], 'Class_Name': [class_name]})], ignore_index=True)
 
                 # Append detection results to the DataFrame
                 row = [captured_time, class_name, conf, x1, y1, x2, y2, 0, 'NaN', 'NaN']
                 results_df.loc[len(results_df)] = row
                 
-    return results_df
+    road_coordinates = road_coordinates.sort_values(by = ['X1', 'Y1'])
+    return results_df, road_coordinates
+
+def collect_coordinates(results, frame, mapping, road_coordinates):
+    for detections in results:
+        if detections is not None:
+            for x1, y1, x2, y2, conf, cls in detections:
+                input_size = (640,640)
+                original_height, original_width, _ = frame.shape
+                x1 = int(x1 * (original_width / input_size[0]))
+                y1 = int(y1 * (original_height / input_size[1]))
+                x2 = int(x2 * (original_width / input_size[0]))
+                y2 = int(y2 * (original_height / input_size[1]))
+                
+                class_name = mapping[int(cls)]
+                
+                if(class_name in ['car', 'truck', 'bus']):
+                    row = [x1, y1, x2, y2, class_name]
+                    road_coordinates.loc[len(road_coordinates)] = row
+                    
+    return road_coordinates
--- yolo/detection.py
+++ yolo/detection.py
@@ -1,70 +1,88 @@
-def category(object_coords):
-    for i in range(len(object_coords)):        
-        if object_coords['Class_Name'][i] == 'person':
-            for j in range(len(object_coords)):
-                px1 = object_coords['X1'][i]
-                py1 = object_coords['Y1'][i]
-                px2 = object_coords['X2'][i]
-                py2 = object_coords['Y2'][i]
-                bx1 = object_coords['X1'][j]
-                by1 = object_coords['Y1'][j]
-                bx2 = object_coords['X2'][j]
-                by2 = object_coords['Y2'][j]
+def check_coordinates(x1,y1,x2,y2, road_coordinates):
+    for i in range(len(road_coordinates)):
+        rx1 = road_coordinates['X1'][i]
+        ry1 = road_coordinates['Y1'][i]
+        rx2 = road_coordinates['X2'][i]
+        ry2 = road_coordinates['Y2'][i]
+        state = 0
+        
+        if rx1 <= x1 <= rx2 and ry1 <= y1 <= ry2:
+            state = 1
+    
+    return state
 
-                # bicycle and motocycle detection
-                if object_coords['Class_Name'][j] in ['bicycle', 'motorcycle']:
-                    if bx1 <= px1 <= bx2 and by1 <= py1 <= by2:
-                        object_coords.at[j, 'action_detection'] = 1
-                        object_coords.at[j, 'action_category'] = f'{object_coords["Class_Name"][j]} detected with person'
-                        if object_coords['Class_Name'][j] == 'bicycle':
-                            object_coords.at[j, 'event_type'] = 'B01'
-                        else:
-                            object_coords.at[j, 'event_type'] = 'B02'
-                    elif bx1 <= px2 <= bx2 and by1 <= py2 <= by2:
-                        object_coords.at[j, 'action_detection'] = 1
-                        object_coords.at[j, 'action_category'] = f'{object_coords["Class_Name"][j]} detected with person'
-                        if object_coords['Class_Name'][j] == 'bicycle':
-                            object_coords.at[j, 'event_type'] = 'B01'
-                        else:
-                            object_coords.at[j, 'event_type'] = 'B02'
+def category(object_coords, road_coordinates):
+    for i in range(len(object_coords)):
+        px1 = object_coords['X1'][i]
+        py1 = object_coords['Y1'][i]
+        px2 = object_coords['X2'][i]
+        py2 = object_coords['Y2'][i]
+        
+        state = check_coordinates(px1, py1, px2, py2, road_coordinates)
+        if state == 1:
+            if object_coords['Class_Name'][i] == 'person':
+                for j in range(len(object_coords)):
+                    bx1 = object_coords['X1'][j]
+                    by1 = object_coords['Y1'][j]
+                    bx2 = object_coords['X2'][j]
+                    by2 = object_coords['Y2'][j]
 
-                # person detection which printed on vehicle
-                elif object_coords['Class_Name'][j] in ['car', 'truck', 'bus']:
-                    if bx1 <= px1 <= bx2 and by1 <= py1 <= by2:
-                        continue # nothing happened
-                    elif bx1 <= px2 <= bx2 and by1 <= py2 <= by2:
-                        continue # nothing happened
+                    # bicycle and motocycle detection
+                    if object_coords['Class_Name'][j] in ['bicycle', 'motorcycle']:
+                        if bx1 <= px1 <= bx2 and by1 <= py1 <= by2:
+                            object_coords.at[j, 'action_detection'] = 1
+                            object_coords.at[j, 'action_category'] = f'{object_coords["Class_Name"][j]} detected with person'
+                            if object_coords['Class_Name'][j] == 'bicycle':
+                                object_coords.at[j, 'event_type'] = 'B01'
+                            else:
+                                object_coords.at[j, 'event_type'] = 'B02'
+                        elif bx1 <= px2 <= bx2 and by1 <= py2 <= by2:
+                            object_coords.at[j, 'action_detection'] = 1
+                            object_coords.at[j, 'action_category'] = f'{object_coords["Class_Name"][j]} detected with person'
+                            if object_coords['Class_Name'][j] == 'bicycle':
+                                object_coords.at[j, 'event_type'] = 'B01'
+                            else:
+                                object_coords.at[j, 'event_type'] = 'B02'
 
-                # person detected
-                else:
-                    object_coords.at[i, 'action_detection'] = 1
-                    object_coords.at[i, 'action_category'] = 'person detected'
-                    object_coords.at[i, 'event_type'] = 'P01'
+                    # person detection which printed on vehicle
+                    elif object_coords['Class_Name'][j] in ['car', 'truck', 'bus']:
+                        if bx1 <= px1 <= bx2 and by1 <= py1 <= by2:
+                            print('on the vehicle')
+                            continue # nothing happened
+                        elif bx1 <= px2 <= bx2 and by1 <= py2 <= by2:
+                            print('on the vehicle')
+                            continue # nothing happened
 
-        elif object_coords['Class_Name'][i] in ['fire', 'smoke', 'car fire']:
-            object_coords.at[i, 'action_detection'] = 1
-            if object_coords['Class_Name'][i] == 'fire':
-                object_coords.at[i, 'action_category'] = 'fire detected'
-                object_coords.at[i, 'event_type'] = 'F01'
-            elif object_coords['Class_Name'][i] == 'smoke':
-                object_coords.at[i, 'action_category'] = 'smoke detected'
-                object_coords.at[i, 'event_type'] = 'F02'
-            elif object_coords['Class_Name'][i] == 'car fire':
-                object_coords.at[i, 'action_category'] = 'car fire detected'
-                object_coords.at[i, 'event_type'] = 'F03'
+                    # person detected
+                    else:
+                        object_coords.at[i, 'action_detection'] = 1
+                        object_coords.at[i, 'action_category'] = 'person detected'
+                        object_coords.at[i, 'event_type'] = 'P01'
 
-        elif object_coords['Class_Name'][i] in ['cat', 'deer', 'dog', 'racoon', 'wild_boar']:
-            object_coords.at[i, 'action_detection'] = 1
-            if object_coords['Class_Name'][i] in ['cat', 'dog']:
-                object_coords.at[i, 'action_category'] = 'animal detected(cat/dog)'
-                object_coords.at[i, 'event_type'] = 'A01'
-            elif object_coords['Class_Name'][i] == 'deer':
-                object_coords.at[i, 'action_category'] = 'animal detected(deer/elk)'
-                object_coords.at[i, 'event_type'] = 'A02'
-            elif object_coords['Class_Name'][i] == 'racoon':
-                object_coords.at[i, 'action_category'] = 'animal detected(racoon)'
-                object_coords.at[i, 'event_type'] = 'A03'
-            elif object_coords['Class_Name'][i] == 'wild_boar':
-                object_coords.at[i, 'action_category'] = 'animal detected(wild_boar/pig)'
-                object_coords.at[i, 'event_type'] = 'A04'
+            elif object_coords['Class_Name'][i] in ['fire', 'smoke', 'car fire']:
+                object_coords.at[i, 'action_detection'] = 1
+                if object_coords['Class_Name'][i] == 'fire':
+                    object_coords.at[i, 'action_category'] = 'fire detected'
+                    object_coords.at[i, 'event_type'] = 'F01'
+                elif object_coords['Class_Name'][i] == 'smoke':
+                    object_coords.at[i, 'action_category'] = 'smoke detected'
+                    object_coords.at[i, 'event_type'] = 'F02'
+                elif object_coords['Class_Name'][i] == 'car fire':
+                    object_coords.at[i, 'action_category'] = 'car fire detected'
+                    object_coords.at[i, 'event_type'] = 'F03'
+
+            elif object_coords['Class_Name'][i] in ['cat', 'deer', 'dog', 'racoon', 'wild_boar']:
+                object_coords.at[i, 'action_detection'] = 1
+                if object_coords['Class_Name'][i] in ['cat', 'dog']:
+                    object_coords.at[i, 'action_category'] = 'animal detected(cat/dog)'
+                    object_coords.at[i, 'event_type'] = 'A01'
+                elif object_coords['Class_Name'][i] == 'deer':
+                    object_coords.at[i, 'action_category'] = 'animal detected(deer/elk)'
+                    object_coords.at[i, 'event_type'] = 'A02'
+                elif object_coords['Class_Name'][i] == 'racoon':
+                    object_coords.at[i, 'action_category'] = 'animal detected(racoon)'
+                    object_coords.at[i, 'event_type'] = 'A03'
+                elif object_coords['Class_Name'][i] == 'wild_boar':
+                    object_coords.at[i, 'action_category'] = 'animal detected(wild_boar/pig)'
+                    object_coords.at[i, 'event_type'] = 'A04'
     return object_coords
