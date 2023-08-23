9b22c5e2c45578a8477f5cb5b31b6788f2a6cf43
yerinnne <128336889+yerinnne@users.noreply.github.com>
2023-08-23 16:27:43 +0900

    modify file

--- FASTAPI_mysql/router/cctv_router.py
+++ FASTAPI_mysql/router/cctv_router.py
@@ -1,13 +1,22 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, File
 from model.user_model import *
 from repository import cctv_repo
 from resource.database import get_db
 from sqlalchemy.orm import Session
 from typing import List
+import os
 
 
 user_router = APIRouter()
 
+# #업로드된 jpg 파일을 저장할 디렉토리 경로
+# upload_dir = 'uploads'
+
+# # 업로드된 파일을 저장하기 위한 디렉토리 생성
+# if not os.path.exists(upload_dir):
+#     os.makedirs(upload_dir)
+
 @user_router.post("/signup")
-def signup(user: cctv_events, db: Session=Depends(get_db)):
+def signup(user: cctv_events, db: Session = Depends(get_db)):
     return cctv_repo.insert(db=db, events = user)
+
