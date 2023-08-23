074339746f2a0facc710f6ed40cd08d544b1d04b
BONGEUNGU <rnqhsrms98@gmail.com>
2023-08-23 12:07:36 +0900

    detection type list up

--- yolo/detection.py
+++ yolo/detection.py
@@ -12,6 +12,12 @@ def check_coordinates(x1,y1,x2,y2, road_coordinates):
     return state
 
 def category(object_coords, road_coordinates):
+    human_detect = ['P01']
+    bike_detect = ['B01', 'B02']
+    fire_detect = ['F01', 'F02', 'F03']
+    animal_detect = ['A01', 'A02', 'A03', 'A04']
+    obstacle_detect = ['S01', 'S02', 'S03', 'S04', 'S05']
+    
     for i in range(len(object_coords)):
         # set coordinates
         px1 = object_coords['X1'][i]
@@ -37,16 +43,16 @@ def category(object_coords, road_coordinates):
                             object_coords.at[j, 'action_detection'] = 1
                             object_coords.at[j, 'action_category'] = f'{object_coords["Class_Name"][j]} detected with person'
                             if object_coords['Class_Name'][j] == 'bicycle':
-                                object_coords.at[j, 'event_type'] = 'B01'
+                                object_coords.at[j, 'event_type'] = bike_detect[0]
                             else:
-                                object_coords.at[j, 'event_type'] = 'B02'
+                                object_coords.at[j, 'event_type'] = bike_detect[1]
                         elif bx1 <= px2 <= bx2 and by1 <= py2 <= by2:
                             object_coords.at[j, 'action_detection'] = 1
                             object_coords.at[j, 'action_category'] = f'{object_coords["Class_Name"][j]} detected with person'
                             if object_coords['Class_Name'][j] == 'bicycle':
-                                object_coords.at[j, 'event_type'] = 'B01'
+                                object_coords.at[j, 'event_type'] = bike_detect[0]
                             else:
-                                object_coords.at[j, 'event_type'] = 'B02'
+                                object_coords.at[j, 'event_type'] = bike_detect[1]
 
                     # person detection which printed on vehicle
                     elif object_coords['Class_Name'][j] in ['car', 'truck', 'bus']:
@@ -61,7 +67,7 @@ def category(object_coords, road_coordinates):
                     else:
                         object_coords.at[i, 'action_detection'] = 1
                         object_coords.at[i, 'action_category'] = 'person detected'
-                        object_coords.at[i, 'event_type'] = 'P01'
+                        object_coords.at[i, 'event_type'] = human_detect[0]
 
             # fire detected
             elif object_coords['Class_Name'][i] in ['fire', 'smoke', 'car fire']:
@@ -70,17 +76,17 @@ def category(object_coords, road_coordinates):
                 # fire detected
                 if object_coords['Class_Name'][i] == 'fire':
                     object_coords.at[i, 'action_category'] = 'fire detected'
-                    object_coords.at[i, 'event_type'] = 'F01'
+                    object_coords.at[i, 'event_type'] = fire_detect[0]
                 
                 # smoke detected
                 elif object_coords['Class_Name'][i] == 'smoke':
                     object_coords.at[i, 'action_category'] = 'smoke detected'
-                    object_coords.at[i, 'event_type'] = 'F02'
+                    object_coords.at[i, 'event_type'] = fire_detect[1]
                 
                 # car fire detected
                 elif object_coords['Class_Name'][i] == 'car fire':
                     object_coords.at[i, 'action_category'] = 'car fire detected'
-                    object_coords.at[i, 'event_type'] = 'F03'
+                    object_coords.at[i, 'event_type'] = fire_detect[2]
 
             # animals detected
             elif object_coords['Class_Name'][i] in ['cat', 'deer', 'dog', 'racoon', 'wild_boar']:
@@ -88,22 +94,22 @@ def category(object_coords, road_coordinates):
                 # cat & dog detected
                 if object_coords['Class_Name'][i] in ['cat', 'dog']:
                     object_coords.at[i, 'action_category'] = 'animal detected(cat/dog)'
-                    object_coords.at[i, 'event_type'] = 'A01'
+                    object_coords.at[i, 'event_type'] = animal_detect[0]
                 
                 # deer & elk detected
                 elif object_coords['Class_Name'][i] == 'deer':
                     object_coords.at[i, 'action_category'] = 'animal detected(deer/elk)'
-                    object_coords.at[i, 'event_type'] = 'A02'
+                    object_coords.at[i, 'event_type'] = animal_detect[1]
                 
                 # racoon detected
                 elif object_coords['Class_Name'][i] == 'racoon':
                     object_coords.at[i, 'action_category'] = 'animal detected(racoon)'
-                    object_coords.at[i, 'event_type'] = 'A03'
+                    object_coords.at[i, 'event_type'] = animal_detect[2]
                 
                 # wild boar & pig detected
                 elif object_coords['Class_Name'][i] == 'wild_boar':
                     object_coords.at[i, 'action_category'] = 'animal detected(wild_boar/pig)'
-                    object_coords.at[i, 'event_type'] = 'A04'
+                    object_coords.at[i, 'event_type'] = animal_detect[3]
             
             # obstacle detected
             elif object_coords['Calss_Name'][i] in ['tree', 'rock', 'box', 'tire', 'drum']:
@@ -111,18 +117,18 @@ def category(object_coords, road_coordinates):
                 # tree detected
                 if(object_coords['Class_Name'][i] == 'tree'):
                     object_coords.at[i, 'action_category'] = 'obstacle detected(tree)'
-                    object_coords.at[i, 'event_type'] = 'S01'
+                    object_coords.at[i, 'event_type'] = obstacle_detect[0]
                 elif(object_coords['Class_Name'][i] == 'rock'):
                     object_coords.at[i, 'action_category'] = 'obstacle detected(rock)'
-                    object_coords.at[i, 'event_type'] = 'S02'
+                    object_coords.at[i, 'event_type'] = obstacle_detect[1]
                 elif(object_coords['Class_Name'][i] == 'box'):
                     object_coords.at[i, 'action_category'] = 'obstacle detected(box)'
-                    object_coords.at[i, 'event_type'] = 'S03'
+                    object_coords.at[i, 'event_type'] = obstacle_detect[2]
                 elif(object_coords['Class_Name'][i] == 'tire'):
                     object_coords.at[i, 'action_category'] = 'obstacle detected(tire)'
-                    object_coords.at[i, 'event_type'] = 'S04'
+                    object_coords.at[i, 'event_type'] = obstacle_detect[3]
                 elif(object_coords['Class_Name'][i] == 'drum'):
                     object_coords.at[i, 'action_category'] = 'obstacle detected(drum)'
-                    object_coords.at[i, 'event_type'] = 'S05'
+                    object_coords.at[i, 'event_type'] = obstacle_detect[4]
     
     return object_coords
