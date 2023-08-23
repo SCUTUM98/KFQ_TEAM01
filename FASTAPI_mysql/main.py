d2c41f2c36f7399ad214b793c70d0fef10e8be40
yerinnne <128336889+yerinnne@users.noreply.github.com>
2023-08-23 09:33:45 +0900

    add and commit all changes

--- FASTAPI_mysql/main.py
+++ FASTAPI_mysql/main.py
@@ -1,9 +1,25 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from router.cctv_router import cctv_router
+from router.cctv_router import user_router
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+import os
 
 app = FastAPI()
 
+# http파일이 있는 static폴더로 경로 설정
+app.mount('/static', StaticFiles(directory='static'), name = 'static')
+
+@app.get("/")
+def read_root():
+    return FileResponse('static/index.html')
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+# 원래는 보안상 이렇게 하면 안됨
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,4 +28,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(cctv_router, prefix="/cctv")
+app.include_router(user_router, prefix="/user")
