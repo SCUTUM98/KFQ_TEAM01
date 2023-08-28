6bdfe211e1f0c2a09756e94f93ca22642a0aa205
yerinnne <128336889+yerinnne@users.noreply.github.com>
2023-08-28 17:52:31 +0900

    change folder

--- FASTAPI_mysql/main.py
+++ FASTAPI_mysql/main.py
@@ -1,23 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from router.cctv_router import user_router
 from fastapi.staticfiles import StaticFiles
-from starlette.responses import FileResponse
-import os
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
-# http파일이 있는 static폴더로 경로 설정
-app.mount('/static', StaticFiles(directory='static'), name = 'static')
-
-@app.get("/")
-def read_root():
-    return FileResponse('/static/index.html')
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
 
 # 원래는 보안상 이렇게 하면 안됨
 app.add_middleware(
@@ -28,4 +15,52 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# html파일이 있는 static폴더로 경로 설정
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
 app.include_router(user_router, prefix="/user")
+
+
+@app.get("/")
+async def home(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("home.html", context)
+
+
+@app.get("/intro1")
+async def intro1(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("intro1.html", context)
+
+
+@app.get("/intro2")
+async def intro2(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("intro2.html", context)
+
+
+@app.get("/charts")
+async def charts(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("charts.html", context)
+
+
+@app.get("/table1")
+async def table1(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("table1.html", context)
+
+
+@app.get("/table2")
+async def table2(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("table2.html", context)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+
